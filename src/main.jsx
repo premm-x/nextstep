276a43353c2f1b1f2c9a88a1e99b37b64aa7b7cc
prem <mybrother1370@gmail.com>
2026-03-01 21:47:42 +0530

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-import { UserProvider } from './config/UserContext'
+import { UserProvider } from './config/userContext'
+
 
 createRoot(document.getElementById('root')).render(
 
